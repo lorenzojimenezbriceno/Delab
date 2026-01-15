ecf976911fa9e44c7befd6beec04a0093c5867a0
Lorenzo Jimenez <ljimenez@datasoftla.com>
2026-01-15 09:33:47 -0600

    Agregada Corporations

---
+++ Delab/Delab.Backend/Controllers/CorporationsController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Delab.AccessData.Data;
+using Delab.Shared.Entities;
+
+namespace Delab.Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CorporationsController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public CorporationsController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Corporations
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Corporation>>> GetCorporations()
+        {
+            return await _context.Corporations.ToListAsync();
+        }
+
+        // GET: api/Corporations/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Corporation>> GetCorporation(int id)
+        {
+            var corporation = await _context.Corporations.FindAsync(id);
+
+            if (corporation == null)
+            {
+                return NotFound();
+            }
+
+            return corporation;
+        }
+
+        // PUT: api/Corporations/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCorporation(int id, Corporation corporation)
+        {
+            if (id != corporation.CorporationId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(corporation).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CorporationExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Corporations
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Corporation>> PostCorporation(Corporation corporation)
+        {
+            _context.Corporations.Add(corporation);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCorporation", new { id = corporation.CorporationId }, corporation);
+        }
+
+        // DELETE: api/Corporations/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCorporation(int id)
+        {
+            var corporation = await _context.Corporations.FindAsync(id);
+            if (corporation == null)
+            {
+                return NotFound();
+            }
+
+            _context.Corporations.Remove(corporation);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CorporationExists(int id)
+        {
+            return _context.Corporations.Any(e => e.CorporationId == id);
+        }
+    }
+}
---
+++ Delab/Delab.Helpers/FileStorage.cs
@@ -0,0 +1,91 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Delab.Helpers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+
+namespace Vent.Helpers;
+
+public class FileStorage : IFileStorage
+{
+    private readonly string connectionString;
+
+    public FileStorage(IConfiguration configuration)
+    {
+        connectionString = configuration.GetConnectionString("AzureStorage")!;
+    }
+
+    // Para Manejo de Imagenes en AZURE Containers
+
+    public async Task RemoveFileAsync(string path, string containerName)
+    {
+        var client = new BlobContainerClient(connectionString, containerName);
+        await client.CreateIfNotExistsAsync();
+        var fileName = Path.GetFileName(path);
+        var blob = client.GetBlobClient(fileName);
+        await blob.DeleteIfExistsAsync();
+    }
+
+    public async Task<string> SaveFileAsync(byte[] content, string extention, string containerName)
+    {
+        var client = new BlobContainerClient(connectionString, containerName);
+        await client.CreateIfNotExistsAsync();
+        client.SetAccessPolicy(PublicAccessType.Blob);
+        var fileName = $"{Guid.NewGuid()}{extention}";
+        var blob = client.GetBlobClient(fileName);
+
+        using (var ms = new MemoryStream(content))
+        {
+            await blob.UploadAsync(ms);
+        }
+        // Es para obtener la url completa junto con el archivo
+        // return blob.Uri.ToString();
+        return fileName;
+    }
+
+    public async Task<string> UploadImage(IFormFile imageFile, string ruta, string guid)
+    {
+        var file = guid;
+        var path = Path.Combine(
+            Directory.GetCurrentDirectory(),
+            ruta,
+            file);
+
+        using (var stream = new FileStream(path, FileMode.Create))
+        {
+            await imageFile.CopyToAsync(stream);
+        }
+
+        return $"{file}";
+    }
+
+    public async Task<string> UploadImage(byte[] imageFile, string ruta, string guid)
+    {
+        var file = guid;
+        var path = Path.Combine(
+            Directory.GetCurrentDirectory(),
+            ruta,
+            file);
+
+        var NIformFile = new MemoryStream(imageFile);
+        using (var stream = new FileStream(path, FileMode.Create))
+        {
+            await NIformFile.CopyToAsync(stream);
+        }
+
+        return $"{file}";
+    }
+
+    public bool DeleteImage(string ruta, string guid)
+    {
+        string path;
+        path = Path.Combine(
+            Directory.GetCurrentDirectory(),
+            ruta,
+            guid);
+
+        File.Delete(path);
+
+        return true;
+    }
+}
